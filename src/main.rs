65320843552272880a74a0faa719e15e607da2bf
Braxon Fair <hello@taxborn.com>
2020-02-24 13:15:12 -0600

    Allow user to specify which file they want through arguments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
-use std::io::{BufRead, BufReader};
+use std::env;
 use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() {
-    let f = BufReader::new(File::open("chess.4dc").expect("Open failed"));
+    let args: Vec<String> = env::args().collect();
 
-    for line in f.lines() {
-        for c in line.expect("lines failed").chars(){
-            println!("Char: {}", c);
+    match args.len() {
+        2 => {
+            let filename = &args[1];
+
+            let f = BufReader::new(File::open(filename).expect("Open failed"));
+
+            for line in f.lines() {
+                for c in line.expect("lines failed").chars(){
+                    println!("Char: {}", c);
+                }
+            }
+        },
+        _ => {
+            println!("Please specify ONE file to compile.");
         }
     }
 }
