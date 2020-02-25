83a8f2f34cb2b540b51bf8100950d32b520f4bf2
Braxon Fair <hello@taxborn.com>
2020-02-25 13:02:50 -0600

    Add parser function to parse given opcodes into instructions

--- src/main.rs
+++ src/main.rs
@@ -71,6 +71,61 @@ fn lexer (source: String) -> Vec<OpCode> {
     operations
 }
 
+fn parser(opcodes: Vec<OpCode>) -> Vec<Instruction> {
+    let mut program: Vec<Instruction> = Vec::new();
+    let mut loop_stack = 0;
+    let mut loop_start = 0;
+
+    for (i, op) in opcodes.iter().enumerate() {
+        if loop_stack == 0 { 
+            let instruct = match op {
+                OpCode::IncrementX => Some(Instruction::IncrementX),
+                OpCode::DecrementX => Some(Instruction::DecrementX),
+                OpCode::IncrementY => Some(Instruction::IncrementY),
+                OpCode::DecrementY => Some(Instruction::DecrementY),
+                OpCode::IncrementZ => Some(Instruction::IncrementZ),
+                OpCode::DecrementZ => Some(Instruction::DecrementZ),
+                OpCode::IncrementW => Some(Instruction::IncrementW),
+                OpCode::DecrementW => Some(Instruction::DecrementW),
+
+                OpCode::LoopBegin => {
+                    loop_start = i;
+                    loop_stack += 1;
+
+                    None
+                },
+
+                OpCode::LoopEnd => panic!("Loop ending at #{} has no beginning", i),
+            };
+
+            match instruct {
+                Some(instruct) => program.push(instruct),
+                None => (),
+            }
+        } else {
+            match op {
+                OpCode::LoopBegin => loop_stack += 1,
+
+                OpCode::LoopEnd => {
+                    loop_stack -= 1;
+
+                    if loop_stack == 0 {
+                        program.push(Instruction::Loop(parser(opcodes[loop_start+1 .. i].to_vec())));
+                    }
+                },
+
+                _ => (),
+            }
+        }
+    }
+
+    if loop_stack != 0 {
+        panic!("Loop that starts at #{} has no matching ending!", loop_start);
+    }
+
+    program
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
