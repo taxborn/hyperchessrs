c4c839a2b2ac56ee2233d42f0ff3be7d30ad0f3b
Braxon Fair <hello@taxborn.com>
2020-02-26 20:03:35 -0600

    Fixed tape not being 8x8x8x8

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,11 @@ fn main() {
 
     let opcodes = lex(source);
     let program = parse(opcodes);
-    let mut tape: Vec<Vec<Vec<Vec<u8>>>> = vec![vec![vec![vec![0u8, 8]; 8]; 8]; 8];
+    let mut tape: Vec<Vec<Vec<Vec<u8>>>> = vec![vec![vec![vec![0u8; 8]; 8]; 8]; 8];
+
+    // println!("{:#?}", tape);
+
+    // let mut pointers: Vec<u8> = vec![0u8; 4];
 
     run(&program, &mut tape, &mut 0, &mut 0, &mut 0, &mut 0);
 }
